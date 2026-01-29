62d94d736ea8d939eb2a3cb0715ee629c3facb21
Samur <107584285+Samur000@users.noreply.github.com>
2026-01-29 16:17:24 +0300

    SDVIG V-2.1.2 - FIX

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-const base = '/SDVIG-TEST2/';
+const base = '/SDVIG/';
 
 export default defineConfig({
 	base,
